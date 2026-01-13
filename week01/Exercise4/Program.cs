2745277084c6a3a571c3077489a8f8fc9c4891f8
Fernando <wmendezgarcia@byupathway.edu>
2026-01-13 09:37:58 -0400

    feat: Finish Exercise 4 to Week 1.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        bool addNumber = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (addNumber)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                addNumber = false;
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
