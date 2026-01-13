727bb226e3ebe548123add6279974a1d75011300
Fernando <wmendezgarcia@byupathway.edu>
2026-01-13 09:16:27 -0400

    feat: Finish Exercise 3 to Week 1.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guessNumber = -1;
+            int guesses = 0;
+
+            while (guessNumber != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (magicNumber > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! Your guesses are {guesses}");
+                }
+
+            }
+            Console.WriteLine("Do you want to play again? (Yes/No) ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer != "yes")
+            {
+                playAgain = false;
+            }
+        }
     }
 }
