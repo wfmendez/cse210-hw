dc334c2ed61c0cdb456f412d588eaa3cfeb5634e
Fernando Mendez <wuillian.f.mendez@gmail.com>
2026-04-01 20:13:12 -0400

    W04 Assignment: Online Ordering Program. Add new classes for Address, Customer, Order, and Product in the OnlineOrdering module

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,25 @@
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _stateProvince;
+    private string _country;
+
+    public Address(string street, string city, string stateProvince, string country)
+    {
+        _street = street;
+        _city = city;
+        _stateProvince = stateProvince;
+        _country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return _country == "USA";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_street}\n{_city}, {_stateProvince}\n{_country}";
+    }
+}
---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,59 @@
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        // Add shipping cost
+        if (_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+
+        foreach (Product product in _products)
+        {
+            label += $"{product.GetName()} ({product.GetProductId()})\n";
+        }
+
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        string label = "";
+        label += $"{_customer.GetName()}\n";
+        label += _customer.GetAddress().GetFullAddress();
+
+        return label;
+    }
+}
